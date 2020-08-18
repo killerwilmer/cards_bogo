364072ff2cc8ed09fb2b063abf6c5520d5783b2c
Wilmer Arteaga <killerwilmer@gmail.com>
2020-08-17 20:53:51 -0500

    Delete and update card

--- src/main/java/com/killerwilmer/cards/CardsApplication.java
+++ src/main/java/com/killerwilmer/cards/CardsApplication.java
@@ -2,8 +2,10 @@ package com.killerwilmer.cards;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.config.EnableMongoAuditing;
 
 @SpringBootApplication
+@EnableMongoAuditing
 public class CardsApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/killerwilmer/cards/controller/CardController.java
+++ src/main/java/com/killerwilmer/cards/controller/CardController.java
@@ -1,10 +1,14 @@
 package com.killerwilmer.cards.controller;
 
+import com.killerwilmer.cards.exception.CardNotFoundException;
 import com.killerwilmer.cards.model.Card;
 import com.killerwilmer.cards.repository.CardRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 
 @RestController
@@ -27,5 +31,37 @@ public class CardController {
 
         Card card = cardRepository.save(new Card(pCard.getName(), pCard.getBody(), pCard.getArea(), pCard.getChannel()));
         return card;
+    }
+
+    @GetMapping("/cards/{id}")
+    Card getOneCard(@PathVariable String id) {
+
+        return cardRepository.findById(id)
+                .orElseThrow(() -> new CardNotFoundException(id));
+    }
+
+    @DeleteMapping("/cards/{id}")
+    void deleteCard(@PathVariable String id) {
+        Card card = cardRepository.findById(id)
+                .orElseThrow(() -> new CardNotFoundException(id));
+
+        cardRepository.deleteById(card.getId());
+    }
+
+    @PutMapping("/cards/{id}")
+    public ResponseEntity<Card> updateCard(@PathVariable("id") String id, @RequestBody Card p_Card) {
+
+        Optional<Card> cardData = cardRepository.findById(id);
+
+        if (cardData.isPresent()) {
+            Card _card = cardData.get();
+            _card.setName(p_Card.getName());
+            _card.setBody(p_Card.getBody());
+            _card.setArea(p_Card.getArea());
+            _card.setChannel(p_Card.getChannel());
+            return new ResponseEntity<>(cardRepository.save(_card), HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
     }
 }
